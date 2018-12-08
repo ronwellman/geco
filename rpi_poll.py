d37f220e20a7ac7fe0513e2c0aabf449d4c820a2
ronwellman <ron.wellman01@gmail.com>
2018-12-08 10:28:02 -0500

    PEP8 fixes.

--- rpi_poll.py
+++ rpi_poll.py
@@ -1,12 +1,19 @@
 #!/usr/bin/env python3
+"""This script is used to log into an old D-Link DGL-4500 circa 2013 wireless
+router using the requests library.  It then scrapes the wireless hosts status
+screen extracting the host information via BeautifulSoup.  Finally, it then
+displays the results using the curses library.  This makes it easy to find all
+of the expected Raspberry Pi's (RPIs) as they connect to the access point.
+"""
 import curses
 from curses import wrapper
-import requests
-from requests.exceptions import ConnectionError, Timeout
 from hashlib import md5
 import re
-from bs4 import BeautifulSoup as bs
 import time
+import requests
+from requests.exceptions import ConnectionError as ReqConnectionError
+from requests.exceptions import Timeout
+from bs4 import BeautifulSoup as bs
 
 
 class InvalidCredentials(Exception):
@@ -18,28 +25,30 @@ def build_payload(text):
     '''
     The below procedures generate the required payload to login to the DGL-4500
     Gaming router (Firmware: 1.24WW,  2013/08/02) used in the geco girls lab.
+
+    build_payload(str) -> str
     '''
     # auth_id is unique every login
-    m = re.search('auth_id=(\w+)', text)
-    auth_id = m.group(1)
+    match = re.search(r'auth_id=(\w+)', text)
+    auth_id = match.group(1)
 
     # salt is unique every login
-    m = re.search('var salt = "(\w+)"', text)
-    salt = m.group(1)
+    match = re.search(r'var salt = "(\w+)"', text)
+    salt = match.group(1)
 
     password = salt + 'just4mentors'
     password += chr(1) * (64 - len(password))
-    hash = md5(password.encode('utf-8')).hexdigest()
+    pw_hash = md5(password.encode('utf-8')).hexdigest()
 
-    hash = salt + hash
-    payload = {'hash': hash, 'auth_code': '', 'auth_id': auth_id}
+    pw_hash = salt + hash
+    payload = {'hash': pw_hash, 'auth_code': '', 'auth_id': auth_id}
     return payload
 
 
-def get_page(s, URL, payload=None):
+def get_page(session, url, payload=None):
     '''
     General purpose function for executing a get request on the session given a
-    URL and potentially a payload.  Rather than passing the payload across, it
+    url and potentially a payload.  Rather than passing the payload across, it
     is used to build a custom get string.
     '''
     authdata = ''
@@ -48,9 +57,9 @@ def get_page(s, URL, payload=None):
         authdata = '?' + 'hash=' + payload['hash'] + '&' +\
             'auth_code=&auth_id=' + payload['auth_id']
 
-    r = s.get(URL + authdata, timeout=.25)
+    result = session.get(url + authdata, timeout=.25)
 
-    return r
+    return result
 
 
 def update_clients():
@@ -58,38 +67,28 @@ def update_clients():
     Builds a requests session object and attempts to query the router for the
     wireless clients.
     '''
-    URL = 'http://192.168.0.1/'
-    POST_URL = 'http://192.168.0.1/post_login.xml'
+    url = 'http://192.168.0.1/'
+    post_url = 'http://192.168.0.1/post_login.xml'
 
-    with requests.Session() as s:
+    with requests.Session() as session:
         # initial page holds aspects needed to generate login payload
-        r = get_page(s, URL)
-        payload = build_payload(r.text)
-        r = get_page(s, POST_URL, payload)
-        if 'Basic/Internet.shtml' in r.text:
-            r = get_page(s, URL + 'wifi_assoc.xml')
+        result = get_page(session, url)
+        payload = build_payload(result.text)
+        result = get_page(session, post_url, payload)
+        if 'Basic/Internet.shtml' in result.text:
+            result = get_page(session, url + 'wifi_assoc.xml')
         else:
             raise InvalidCredentials
 
-    if 'assoc' in r.text:
-        return r.text
+    if 'assoc' in result.text:
+        return result.text
 
     return None
 
 
-def main(stdscr):
-    rpi = {'B8:27:EB:0A:29:BE': {'NAME': 'raspberrypi', 'IP': ''},
-           'B8:27:EB:6C:34:70': {'NAME': 'applepi', 'IP': ''},
-           'B8:27:EB:BD:81:35': {'NAME': 'strawberrypi', 'IP': ''},
-           '00:0F:60:08:1F:04': {'NAME': 'cherrypi', 'IP': ''},
-           'B8:27:EB:4A:AA:65': {'NAME': 'bannapi', 'IP': ''},
-           'B8:27:EB:01:FB:49': {'NAME': 'lemonpi', 'IP': ''},
-           'B8:27:EB:DC:69:19': {'NAME': 'pecanpi', 'IP': ''},
-           'B8:27:EB:60:CB:0B': {'NAME': 'blueberrypi', 'IP': ''},
-           'B8:27:EB:F2:28:37': {'NAME': 'blackberrypi', 'IP': ''},
-           'B8:27:EB:B6:D2:E6': {'NAME': 'keylimpi', 'IP': ''},
-           'B8:27:EB:E7:9D:3A': {'NAME': 'cutiepi', 'IP': ''}}
-
+def initialize_ncurses():
+    """Initializes ncurses.
+    """
     # initialize ncurses
     stdscr = curses.initscr()
     curses.start_color()
@@ -106,8 +105,27 @@ def main(stdscr):
     curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_GREEN)
     curses.init_pair(4, curses.COLOR_WHITE, curses.COLOR_RED)
 
-    loop = True
-    while(loop):
+    return stdscr
+
+
+def main(stdscr):
+    """Handles polling of the wireless router and display of the results.
+    """
+    rpi = {'B8:27:EB:0A:29:BE': {'NAME': 'raspberrypi', 'IP': ''},
+           'B8:27:EB:6C:34:70': {'NAME': 'applepi', 'IP': ''},
+           'B8:27:EB:BD:81:35': {'NAME': 'strawberrypi', 'IP': ''},
+           '00:0F:60:08:1F:04': {'NAME': 'cherrypi', 'IP': ''},
+           'B8:27:EB:4A:AA:65': {'NAME': 'bannapi', 'IP': ''},
+           'B8:27:EB:01:FB:49': {'NAME': 'lemonpi', 'IP': ''},
+           'B8:27:EB:DC:69:19': {'NAME': 'pecanpi', 'IP': ''},
+           'B8:27:EB:60:CB:0B': {'NAME': 'blueberrypi', 'IP': ''},
+           'B8:27:EB:F2:28:37': {'NAME': 'blackberrypi', 'IP': ''},
+           'B8:27:EB:B6:D2:E6': {'NAME': 'keylimpi', 'IP': ''},
+           'B8:27:EB:E7:9D:3A': {'NAME': 'cutiepi', 'IP': ''}}
+
+    stdscr = initialize_ncurses()
+
+    while True:
         line = 0
         clients = None
         soup = None
@@ -162,26 +180,26 @@ def main(stdscr):
         stdscr.addstr(line, col_1, top_bar, curses.A_UNDERLINE)
         line += 1
 
-        for k, v in sorted(rpi.items(), key=lambda x: x[1]['NAME']):
+        for key, value in sorted(rpi.items(), key=lambda x: x[1]['NAME']):
             if line % 2 == 0:
                 color = 1
             else:
                 color = 2
-            name = v['NAME']
-            mac = k
-            ip = v['IP']
+            name = value['NAME']
+            mac = key
+            ip_addr = value['IP']
 
             stdscr.addstr(line, col_1, name, curses.color_pair(color))
             stdscr.addstr(line, col_1 + len(name),
                           ' ' * (col_2 - (col_1 + len(name))),
                           curses.color_pair(color))
-            stdscr.addstr(line, col_2,  mac, curses.color_pair(color))
+            stdscr.addstr(line, col_2, mac, curses.color_pair(color))
             stdscr.addstr(line, col_2 + 17,
                           ' ' * 5,
                           curses.color_pair(color))
-            stdscr.addstr(line, col_3, ip, curses.color_pair(color))
-            stdscr.addstr(line, col_3 + len(ip),
-                          ' ' * (col_4 - (col_3 + len(ip))),
+            stdscr.addstr(line, col_3, ip_addr, curses.color_pair(color))
+            stdscr.addstr(line, col_3 + len(ip_addr),
+                          ' ' * (col_4 - (col_3 + len(ip_addr))),
                           curses.color_pair(color))
             line += 1
 
@@ -189,9 +207,9 @@ def main(stdscr):
         stdscr.addstr(line + 4, 0, "HIT <Q> TO QUIT")
         stdscr.refresh()
 
-        c = stdscr.getch()
-        if c == 113 or c == 81:
-            loop = False
+        char = stdscr.getch()
+        if char == 113 or char == 81:
+            break
 
     return 0
 
