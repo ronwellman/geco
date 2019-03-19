56bff7c2f4e17439c38ede7ba7d0638c71b86f3a
ronwellman <ron.wellman01@gmail.com>
2019-03-18 21:35:59 -0400

    Barebones implementation of a message_board server.

---
+++ message_board/server.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+'''Generic TCP Server for receiving messages and printing them out to the
+screen.
+'''
+import socket
+
+PORT = 5555
+IP = '0.0.0.0'
+
+# build a socket via IPV4 TCP
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# bind on every IPV4 Address via port 5555
+server_socket.bind((IP, PORT))
+
+# listen for incoming connections
+server_socket.listen(10)
+
+try:
+
+    while True:
+        # accept incoming connections
+        client_socket, client_address = server_socket.accept()
+        print(client_address)
+
+        # read in a message
+        message = client_socket.recv(65535)
+
+        print("Message: {}".format(message.decode('utf-8').strip()))
+
+        client_socket.close()
+
+except KeyboardInterrupt:
+    server_socket.close()
