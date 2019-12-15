c69a15fdc3ef067c933709a0d24e84017e8648a5
ronwellman <ron.wellman01@gmail.com>
2019-12-14 22:24:31 -0500

    Upload updates maze_solver

---
+++ maze_solver/README.md
@@ -0,0 +1,115 @@
+# Maze Solver
+In this lab, we will programmatically figure out how to solve an ASCII maze. We
+will not apply any optimizations such as using a shortest path first algorithm
+but will instead perform a quasi depth first search.
+
+![MazeSolver](./images/maze_solver.gif).
+
+## Maze generation
+This [link](https://www.dcode.fr/maze-generator) was used to generate the maps.
+The maps were further modified to close them in completely and place a start
+`S` point as well as end `E` point.
+
+### Sample Maze
+```
+###########
+#S  #     #
+# ##### ###
+# #       #
+# ### #####
+#       # #
+# ##### # #
+# #   #   #
+### ##### #
+#        E#
+###########
+```
+
+* Maze is completely closed in.  
+* Walkways are represented by spaces.  
+* Start is identified by a capital `S`.
+* End is identified by a capital `E`.
+
+## Maze Class API
+
+### Create Maze Object
+`maze = Maze(PATH)` - Requires a valid path to a maze.
+
+### Get Start
+```
+    def get_start(self):
+        '''
+        get_start() -> tuple
+
+        Returns a tuple of the row and column numbers that identifies where 'S'
+        is in the maze.
+        '''
+```
+
+### Get Position Contents
+```
+    def get_point(self, point):
+        '''
+        get_point(tuple) -> str
+
+        Returns the character at the row and column or None if not found.
+        '''
+```
+
+### Print Maze
+```
+    def __str__(self, visited=None, curr_pos=None):
+        '''
+        __str__(set=None, tuple=None) -> str
+
+        Given a set of coordinates and current position, print the current
+        state of the maze.  Otherwise, just print the maze.
+        '''
+ ```
+
+## Class Solver API
+
+### Create Solver Object
+`solver = Solver(maze)` - Requires a maze object.
+
+### Search
+```
+    def search(self):
+        '''
+        _perform_search() -> NoneType
+
+        Continues to move around until all all nodes have been visited.
+        '''
+```
+
+### Query Quadrants
+```
+    def _query_quadrants(self):
+        '''
+        _query_quadrants() -> NoneType
+
+        Peers into the four coordinates around the current position.
+        '''
+```
+
+### Update Seen
+```
+    def _update_seen(self, position):
+        '''
+        _update_seen(tuple) -> NoneType
+
+        Checks the position and updates seen if position is empty.
+        '''
+```
+### Update Position
+```
+    def _update_pos(self, position):
+        '''
+        update_pos(tuple) -> NoneType
+
+        Updates current position.
+        '''
+```
+
+### Print Current Maze
+`print(solver)` - calls `__str__` of maze object and returns string of maze
---
+++ maze_solver/solver.py
@@ -0,0 +1,218 @@
+#!/usr/bin/env python3
+import time
+import os
+import sys
+from maze_support import Maze
+
+
+class Solver:
+    """
+    class Solver attempts to solve a maze. It does not perform any
+    optimizations (shortest path) but instead performs a quasi depth first
+    search until it finds the endpoint.
+    """
+
+    def __init__(self, maze):
+        # store the maze
+        self.maze = maze
+
+        # get the start position
+        self.start = maze.get_start()
+
+        # End is not known
+        self.end = None
+
+        # current position is the start position
+        self.curr_pos = self.start
+
+        # create a visited and seen set (sets will only contain unique entries)
+        # visited (solver has been here)
+        # seen (solver has been near here)
+        self.visited = set()
+        self.seen = set()
+
+        # create a path record of where we have been (list)
+        self.path = [self.start]
+
+        # record that we have visited and seen the start
+        self.visited.add(self.start)
+        self.seen.add(self.start)
+
+        # initially look at what surrounds current position
+        self._query_quadrants()
+    
+    @staticmethod
+    def left(position):
+        """
+        left(tuple) -> tuple
+
+        Returns position moved left by 1.
+        """
+        row, col = position
+        return (row, col - 1)
+
+    @staticmethod
+    def right(position):
+        """
+        right(tuple) -> tuple
+
+        Returns position moved right by 1.
+        """
+        row, col = position
+        return (row, col + 1)
+
+    @staticmethod
+    def up(position):
+        """
+        up(tuple) -> tuple
+
+        Returns position moved up by 1.
+        """
+        row, col = position
+        return (row - 1, col)
+
+    @staticmethod
+    def down(position):
+        """
+        down(tuple) -> tuple
+
+        Returns position moved down by 1.
+        """
+        row, col = position
+        return (row + 1, col)
+
+    def _query_quadrants(self):
+        """
+        _query_quadrants() -> NoneType
+
+        Peers into the four coordinates around the current position.
+        """
+
+        # update seen up, down, left, and right
+        self._update_seen(self.up(self.curr_pos))
+        self._update_seen(self.down(self.curr_pos))
+        self._update_seen(self.left(self.curr_pos))
+        self._update_seen(self.right(self.curr_pos))
+
+    def _update_seen(self, position):
+        """
+        _update_seen(tuple) -> NoneType
+
+        Checks the position and updates seen if position is empty.
+        """
+
+        # query the maze for what is at row, col
+        value = self.maze.get_point(position)
+
+        # Only update seen if position is blank or End point
+        if value == " " or value == "E":
+            self.seen.add(position)
+
+        # if position is End Point, record it in self.end
+        if value == "E":
+            self.end = position
+
+    def search(self):
+        """
+        search() -> NoneType
+
+        Continues to move around until all all nodes have been visited.
+        """
+        while True:
+
+            # determine if we have visited all known positions
+            unvisited = self.seen.difference(self.visited)
+            
+            if len(unvisited) == 0:
+                break
+
+            # choose a direction to go
+            if self.end is not None:
+                self._update_pos(self.end)
+            elif (self.up(self.curr_pos)) in unvisited:
+                self._update_pos(self.up(self.curr_pos))
+            elif (self.down(self.curr_pos)) in unvisited:
+                self._update_pos(self.down(self.curr_pos))
+            elif (self.left(self.curr_pos)) in unvisited:
+                self._update_pos(self.left(self.curr_pos))
+            elif (self.right(self.curr_pos)) in unvisited:
+                self._update_pos(self.right(self.curr_pos))
+            else:
+                # last entry in path is where solver is currently standing
+                self.path.pop()
+
+                # move to previous position
+                self._update_pos(self.path.pop())
+
+            # take a look at surrounding positions
+            self._query_quadrants()
+
+            # take a quick sleep so we can watch the progress of the search
+            time.sleep(0.1)
+
+            # clear the screen
+            if os.name == "nt":
+                os.system("cls")
+            else:
+                os.system("clear")
+
+            # print solver's progress
+            print(self)
+
+            # check to see if we've reached the end
+            if self.end is not None:
+                break
+
+    def _update_pos(self, position):
+        """
+        update_pos(tuple) -> NoneType
+
+        Updates current position.
+        """
+        # update current position
+        self.curr_pos = position
+
+        # add new position to path (list)
+        self.path.append(self.curr_pos)
+
+        # add new position to visited (set)
+        self.visited.add(self.curr_pos)
+
+    def __str__(self):
+        """
+        __str__() -> str
+
+        Print the current state of the maze.
+        """
+        # if maze has been solved, only show path that was taken
+        if self.end is not None:
+            return self.maze.__str__(self.path, self.curr_pos)
+
+        return self.maze.__str__(self.visited, self.curr_pos)
+
+
+if __name__ == "__main__":
+
+    # check to see if a path to the maze was passed to the program
+    if len(sys.argv) != 2:
+        print("Invalid usage:")
+        print("\nmaze_solver.py MAZE")
+        sys.exit(0)
+
+    # create a maze object
+    maze = Maze(sys.argv[1])
+    if maze is None:
+        print("Unable to load maze: {}".format(sys.argv[1]))
+        sys.exit(0)
+
+    # create a solver object
+    solver = Solver(maze)
+
+    # instruct solver to search for the endpoint
+    solver.search()
+
+    # check to see if solver was able to solve the maze
+    if solver.end is None:
+        print("Unable to solve...")
+    else:
+        print("Solved!!!")
